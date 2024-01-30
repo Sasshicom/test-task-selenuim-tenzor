d117b0baaa8ac8a16f3bda02e6481817c9f23c15
Sasshicom <workmail1151A@mail.ru>
2024-01-30 09:32:48 +0700

    Убрал использование абсолютного пути

--- conftest.py
+++ conftest.py
@@ -1,14 +1,16 @@
 from selenium import webdriver
 import pytest
+import os
 
 
 @pytest.fixture()
 def browser():
     options = webdriver.ChromeOptions()
+    working_directory = os.getcwd()
 
     # Определяем опшенсы хрома для того, чтобы можно было выкачивать файлик в нужную себе директорию
     # и игнорировать ошибку непроверенного файла при скачивании
-    prefs = {"download.default_directory": r'D:\pythonProject\test-task-selenuim-tenzor',
+    prefs = {"download.default_directory": working_directory,
              'safebrowsing.enabled': True}
     options.add_experimental_option("prefs", prefs)
     browser = webdriver.Chrome(options=options)
